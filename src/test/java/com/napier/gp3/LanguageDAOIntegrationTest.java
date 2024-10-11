23a4cd32f4348e95a254e370e5076ac6722659fa
AungChan40685540 <40685540@live.napier.ac.uk>
2024-10-11 09:43:15 +0630

    Refactor languageDAO integration

--- src/test/java/com/napier/gp3/LanguageDAOIntegrationTest.java
+++ src/test/java/com/napier/gp3/LanguageDAOIntegrationTest.java
@@ -33,11 +33,14 @@ public class LanguageDAOIntegrationTest {
         languageDAO = new LanguageDAO(con);
     }
 
+    /**
+     * test languages by number of people who speak in languages
+     */
     @Test
     public void testGetLanguagesByNumberOfPeople() {
         List<Language> languages = languageDAO.getLanguagesByNumberOfPeople();
-        assertNotNull(languages, "The list of languages should not be null");
-        assertFalse(languages.isEmpty(), "The list of languages should not be empty");
+        assertNotNull(languages, "languages can't be null");
+        assertFalse(languages.isEmpty(), "languages can't be empty");
 
         // Verify that the languages are sorted by the number of people in descending order
         assertTrue(languages.get(0).getNumberOfPeople() >= languages.get(1).getNumberOfPeople(),
